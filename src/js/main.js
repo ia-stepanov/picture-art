fe5058a40711b569db0975b45b1acf854784791b
Igor Stepanov <igorstepanov@hotmail.com>
2023-07-29 17:33:23 +0300

    build(Picture Art): реализовать логику работы слайдеров на странице

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,10 @@
 import modals from './modules/modals.js';
+import sliders from './modules/sliders.js';
 
 window.addEventListener('DOMContentLoaded', () => {
   'use strict';
 
   modals();
+  sliders('.feedback-slider-item', 'horizontal', '.main-next-btn', '.main-prev-btn');
+  sliders('.main-slider-item', 'vertical');
 });
